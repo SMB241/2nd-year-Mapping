ea22d92a07e4885754038b64bdc22c6848e0312d
SMB241 <Mattbuen2805@gmail.com>
2025-09-29 16:33:56 +0800

    Add files via upload

--- map.js
+++ map.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     var map = L.map('map').setView([10.669644, 122.948844], 17);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
     var tempMarker = null;
+    var markers = [];                
 
     function clearForm() {
         $("#lat").val('');
         $("#lng").val('');
         $("#category").val('');
+        $("#name").val('');
+        $("#description").val('');
     }
 
     var customControl = L.control({ position: 'topright' });
@@ -19,7 +23,7 @@ $(document).ready(function () {
         resetBtn.style.display = "block";
         resetBtn.style.width = "100%";
         resetBtn.onclick = function (e) {
-            L.DomEvent.stopPropagation(e); 
+            L.DomEvent.stopPropagation(e);
             map.setView([10.669644, 122.948844], 17);
         };
 
@@ -28,11 +32,84 @@ $(document).ready(function () {
         plotBtn.style.display = "block";
         plotBtn.style.width = "100%";
         plotBtn.onclick = function (e) {
-            L.DomEvent.stopPropagation(e); 
-            clearForm(); 
+            L.DomEvent.stopPropagation(e);
+            clearForm();
             $("#plotForm").show();
         };
 
+        var postBtn = L.DomUtil.create('button', '', div);
+        postBtn.innerHTML = "Post";
+        postBtn.style.display = "block";
+        postBtn.style.width = "100%";
+        postBtn.onclick = function (e) {
+            L.DomEvent.stopPropagation(e);
+            $.ajax({
+                url: "forms/fetch_location.php",
+                type: "GET",
+                success: function (response) {
+                    var res = JSON.parse(response);
+                    if (res.status === "success") {
+                        res.data.forEach(function (loc) {
+                            let lat = parseFloat(loc.latitude);
+                            let lng = parseFloat(loc.longitude);
+                            let desc =loc.description ;
+                            let cat = loc.category;
+                            let name = loc.name;
+
+                            if (tempMarker) {
+                            map.removeLayer(tempMarker);
+                            tempMarker = null;
+                        }
+
+                        let iconpath = '';
+                        if (cat === 'Restaurant') {
+                            iconpath = 'assets/images/restaurant.png';
+                        } else if (cat === 'Church') {
+                            iconpath = 'assets/images/church.png';
+                        } else if (cat === 'Mall') {
+                            iconpath = 'assets/images/mall.png';
+                        } else if (cat === 'School') {
+                            iconpath = 'assets/images/school.png';
+                        } else {
+                            iconpath = 'assets/images/park.png';
+                        }
+
+                        var customIcon = L.icon({
+                            iconUrl: iconpath,
+                            iconSize: [50, 50],
+                            iconAnchor: [25, 50],
+                            popupAnchor: [0, -50]
+                        });
+
+                        var marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
+
+                        marker.bindPopup(
+                            "<b>" + name.toUpperCase() + "</b><br>" + cat + "<br>" + desc,
+                            { autoClose: false, closeOnClick: false }
+                        ).openPopup();
+                            
+                        });
+                    } else {
+                        alert("No locations found.");
+                    }
+                },
+                error: function () {
+                    alert("Error fetching locations.");
+                }
+            });
+        };
+
+        var clearBtn = L.DomUtil.create('button', '', div);
+        clearBtn.innerHTML = "Clear";
+        clearBtn.style.display = "block";
+        clearBtn.style.width = "100%";
+
+        // Code clear routine here...
+        clearBtn.onclick = function (e) {
+            L.DomEvent.stopPropagation(e);
+
+
+        };
         return div;
     };
 
@@ -41,7 +118,6 @@ $(document).ready(function () {
     $("#newBtn").click(function (e) {
         e.stopPropagation();
         clearForm();
-
         if (tempMarker) {
             map.removeLayer(tempMarker);
             tempMarker = null;
@@ -51,7 +127,6 @@ $(document).ready(function () {
     $("#closeForm").click(function (e) {
         e.stopPropagation();
         $("#plotForm").hide();
-
         if (tempMarker) {
             map.removeLayer(tempMarker);
             tempMarker = null;
@@ -89,47 +164,74 @@ $(document).ready(function () {
     });
 
     $("#saveBtn").click(function (e) {
-            e.stopPropagation();
-            var lat = parseFloat($("#lat").val());
-            var lng = parseFloat($("#lng").val());
-            var category = $("#category").val();
+        e.stopPropagation();
+        var lat = parseFloat($("#lat").val());
+        var lng = parseFloat($("#lng").val());
+        var category = $("#category").val();
+        var name = $("#name").val();
+        var description = $("#description").val();
 
-            if(!isNaN(lat) && !isNaN(lng) && category!== ""){
-                $.ajax({
-                    url: "forms/save_location.php",
-                    type: "POST",
-                    data: {
-                        latitude: lat,
-                        longitude: lng,
-                        category: category
-                    },
+        if (!isNaN(lat) && !isNaN(lng) && category !== "") {
+            $.ajax({
+                url: "forms/save_location.php",
+                type: "POST",
+                data: {
+                    latitude: lat,
+                    longitude: lng,
+                    category: category,
+                    name: name,
+                    description: description
+                },
+                success: function (response) {
+                    var res = JSON.parse(response);
 
-                    success: function(response) {
-                        let res = JSON.parse(response);
-                        if(res.status === "success") {
-                            if(tempMarker){
-                                map.removeLayer(tempMarker);
-                                tempMarker = null;
-                            }
-
-                            L.marker([lat,lng])
-                                .addTo(map)
-                                .bindPopup("<b>" + category + "</b> <br>(" + lat + ", " + lng + ")")
-
-                                .openPopup();
-
-                            $("#plotForm").hide();
-                                alert("Saved Successfully");
-                        } else {
-                            alert(res.message);
+                    if (res.status === "success") {
+                        if (tempMarker) {
+                            map.removeLayer(tempMarker);
+                            tempMarker = null;
                         }
-                    },
-                    error: function(){
-                        alert("Error connecting to server.");
+
+                        let iconpath = '';
+                        if (category === 'Restaurant') {
+                            iconpath = 'assets/images/restaurant.png';
+                        } else if (category === 'Church') {
+                            iconpath = 'assets/images/church.png';
+                        } else if (category === 'Mall') {
+                            iconpath = 'assets/images/mall.png';
+                        } else if (category === 'School') {
+                            iconpath = 'assets/images/school.png';
+                        } else {
+                            iconpath = 'assets/images/park.png';
+                        }
+
+                        var customIcon = L.icon({
+                            iconUrl: iconpath,
+                            iconSize: [50, 50],
+                            iconAnchor: [25, 50],
+                            popupAnchor: [0, -50]
+                        });
+
+                        var marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
+
+                        marker.bindPopup(
+                            "<b>" + name.toUpperCase() + "</b><br>" + category + "<br>" + description,
+                            { autoClose: false, closeOnClick: false }
+                        ).openPopup();
+
+                        markers.push(marker);
+
+                        $("#plotForm").hide();
+                        alert("Saved successfully!");
+                    } else {
+                        alert(res.message);
                     }
-                });
-            } else {
-                alert("Please enter valid latitude, longitude, and category.");
-            }
+                },
+                error: function () {
+                    alert("Error connecting to server.");
+                }
+            });
+        } else {
+            alert("Please enter valid latitude, longitude, and category.");
+        }
     });
 });
